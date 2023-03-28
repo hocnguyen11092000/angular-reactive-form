bc7ae522b6c60d9ee09c9addf70241e457eb4e16
ADMIN\Admin <hocnguyen1109200@gmail.com>
2023-03-28 23:51:51 +0700

    nested form array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { SkyFormContainerComponent } from 'src/components/sky-mockup/sky-form-container/sky-form-container.component';
+import { SkyFormItemComponent } from 'src/components/sky-mockup/sky-form-item/sky-form-item.component';
 
 @NgModule({
-  declarations: [AppComponent, InforFormComponent, InforFormContainerComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    InforFormComponent,
+    InforFormContainerComponent,
+    SkyFormContainerComponent,
+    SkyFormItemComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NzTabsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
